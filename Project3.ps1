bf9a628bb1d3d2d96b986631e03d02951e8745d5
rgonzo <rgonzo1355@gmail.com>
2023-12-20 10:09:13 -0700

    1

--- Project3.ps1
+++ Project3.ps1
@@ -1,3 +1,6 @@
+# Script Name:                  PS
+# Author:                       Rodolfo Gonzalez
+# Date of latest revision:      12/20/2023
 # PowerShell script to deploy a Windows Server VM as a Domain Controller for Mistic Technologies Inc.
 # With improved error checking and rollback
 
